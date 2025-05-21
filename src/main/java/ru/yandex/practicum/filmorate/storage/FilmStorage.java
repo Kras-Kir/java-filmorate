57c24fb6807c95263176f6de44d60d6a765efa24
Kirill Krasikov <c.krasikoff@yandex.ru>
2025-05-21 14:29:03 +0300

    ФЗ Спринт 11 Правки после автопроверки

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -1,14 +1,18 @@
 package ru.yandex.practicum.filmorate.storage;
 
-import org.springframework.web.bind.annotation.RequestBody;
+
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.Collection;
 
 public interface FilmStorage {
     Film createFilm(Film film);
+
     Film updateFilm(Film film);
+
     Collection<Film> getAllFilms();
+
     void deleteFilm(long id);
+
     Film getFilmById(long id);
 }
