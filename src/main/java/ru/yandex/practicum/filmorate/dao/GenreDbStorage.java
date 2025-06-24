e53544a668712ce419b82cbf5193555030092465
Kirill Krasikov <c.krasikoff@yandex.ru>
2025-06-24 15:01:09 +0300

    Спринт 12.Первое рьвью1

--- src/main/java/ru/yandex/practicum/filmorate/dao/GenreDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/GenreDbStorage.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate.dao;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import ru.yandex.practicum.filmorate.model.Genre;
